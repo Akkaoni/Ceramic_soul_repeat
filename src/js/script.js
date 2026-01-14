4c648b88aaf067d7942041358c4e09dd749178a8
Artur <akkaoniko@gmail.com>
2026-01-15 01:11:03 +0600

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,45 @@
 import "../sass/style.scss";
+
+// let text = "Hello world";
+// const pi = 3.14;
+// const isOpen = false;
+// let a;
+
+// text = "This is text";
+
+// console.log(text);
+// console.log(a);
+// console.log(isOpen);
+
+// const object = {
+//   name: "Artur",
+//   age: "28",
+// };
+
+// const titles = [
+//   "Make your dream come true or decorate your home",
+//   "create or buy",
+//   "our store",
+//   "our workshop",
+//   "get in touch",
+// ];
+
+// function calc(a, b) {
+//   console.log(a + b);
+// }
+
+// calc(5, 5);
+// calc(10, 12);
+
+// if (isOpen) {
+//   console.log("Shop is open");
+// } else {
+//   console.log("Shop is close");
+// }
+
+// const vase = document.querySelector(".touch__img_vase");
+// console.log(vase);
+
+// vase.addEventListener("click", () => {
+//   console.log(vase);
+// });
